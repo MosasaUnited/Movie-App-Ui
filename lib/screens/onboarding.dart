ed8f6292bfb7a8a13b6650a6175c8e96bfbf7bb3
MosasaUnited <mostafa447@hotmail.com>
2023-06-30 17:17:03 +0300

    ManageGradleFrom 8.1.1 to 7.5

--- lib/screens/onboarding.dart
+++ lib/screens/onboarding.dart
@@ -1,6 +1,7 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:movie_app_ui/components/custom_outline.dart';
 import 'package:movie_app_ui/constants.dart';
 
 class OnBoardingScreen extends StatelessWidget {
@@ -55,6 +56,32 @@ class OnBoardingScreen extends StatelessWidget {
                   ),
                 ),
               ),
+            ),
+            Column(
+              children: [
+                SizedBox(
+                  height: screenHeight * 0.07,
+                ),
+                CustomOutline(
+                  strokeWidth: 4,
+                  radius: screenWidth * .8,
+                  gradient: LinearGradient(
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                    colors: [
+                      Constants.kPinkColor,
+                      Constants.kPinkColor.withOpacity(0),
+                      Constants.kGreenColor.withOpacity(.1),
+                      Constants.kGreenColor,
+                    ],
+                    stops: [.2, .4, .6, 1],
+                  ),
+                  width: screenWidth * .8,
+                  height: screenHeight * .8,
+                  padding: EdgeInsets.all(4),
+                  child: Container(),
+                ),
+              ],
             ),
           ],
         ),
